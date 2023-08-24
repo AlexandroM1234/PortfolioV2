993745108bf0bc1d6ec79e15d5a531db9e5987c1
Alex Martinez <alex@Alexs-MacBook-Air.local>
2023-08-24 16:44:24 -0400

    updated about section adding the tech stack section needs more styling

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -1,11 +1,32 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { TechStack } from "../constants/constants";
 import React from "react";
+import TechCard from "./TechCard";
 
 function About() {
   return (
     <>
-      <Flex justifyContent={"center"} height={"2xl"}>
+      <Flex
+        justifyContent={"center"}
+        height={"2xl"}
+        flexDirection={"column"}
+        alignItems={"center"}
+      >
         <Text fontSize={"4xl"}>About Me</Text>
+
+        <Flex textAlign={"center"} flexDirection={"column"}>
+          <Text>
+            I am a Full-Stack Engineer with a focus on the frontend based out of
+            Virginia. Outside of sitting in my room and coding I like to
+            workout, play video games and listen to music.
+          </Text>
+          <Text>Tech Stack</Text>
+          <SimpleGrid columns={2} spacing={3}>
+            {TechStack.map((item) => (
+              <TechCard name={item.name} icon={item.icon} />
+            ))}
+          </SimpleGrid>
+        </Flex>
       </Flex>
     </>
   );
