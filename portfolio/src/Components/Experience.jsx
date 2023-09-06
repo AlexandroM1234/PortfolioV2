53c429273da8bbee2ae322d93eda708174ae1099
Alex Martinez <alex@Alexs-MacBook-Air.local>
2023-09-06 11:10:14 -0400

    experience section mostly done

--- portfolio/src/Components/Experience.jsx
+++ portfolio/src/Components/Experience.jsx
@@ -1,11 +1,20 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, VStack } from "@chakra-ui/react";
 import React from "react";
+import { JobExperience } from "../constants/constants.js";
+import ExperienceCard from "./ExperienceCard.jsx";
 
 function Experience() {
   return (
-    <Flex height={"2xl"} justifyContent={"center"}>
-      <Text fontSize={"4xl"}>Experience</Text>
-    </Flex>
+    <>
+      <Flex height={"2xl"} align={"center"} direction={"column"}>
+        <Text fontSize={"4xl"}>Experience</Text>
+        <VStack align={"center"} spacing={4}>
+          {JobExperience.map((job) => (
+            <ExperienceCard job={job} />
+          ))}
+        </VStack>
+      </Flex>
+    </>
   );
 }
 
