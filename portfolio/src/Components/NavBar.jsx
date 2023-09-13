9a1b3d2aede1d7dac03b382170ed889ac58f1049
Alex Martinez <alex@Alexs-MacBook-Air.local>
2023-09-13 10:22:53 -0400

    navbar made responsive needs more testing

--- portfolio/src/Components/NavBar.jsx
+++ portfolio/src/Components/NavBar.jsx
@@ -17,18 +17,23 @@ const NavBar = ({ handleScroll, aboutRef, experienceRef, projectsRef }) => {
     <>
       <Flex
         justifyContent="space-between"
-        w="100%"
+        w={"100%"}
         padding={2}
         backgroundColor={colorMode === "dark" ? "gray.900" : "white"}
         position={"fixed"}
         boxShadow={"base"}
         top={0}
       >
-        <Stack marginLeft={2} align={"center"} direction="row">
+        <Stack
+          marginX={1}
+          w={"100%"}
+          align={"center"}
+          direction={["row-reverse", "row-reverse", "row"]}
+        >
           <Avatar showBorder name="Alex Martinez" src="/me.png" />
           <Heading size="md">{MY_NAME}</Heading>
         </Stack>
-        <Stack direction="row">
+        <Stack direction="row" display={["none", "none", "contents"]}>
           <Button
             as={motion.button}
             whileHover={{ scale: 1.1 }}
